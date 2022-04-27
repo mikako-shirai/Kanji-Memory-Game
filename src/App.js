57b6766d2d962b4fd768675299e0f8b5e1ab6b18
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-04-27 14:47:06 +0900

    Feat: create function to compare matches and respond accordingly while preventing duplicate card selection
    Co-authored-by: JonrGull <jonrgull@users.noreply.github.com>
    Co-authored-by: Constantine7s <constantine7s@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import { useState, useEffect } from 'react'
 // import cardList from './cardlist.jsx'
 
-const kanjiArr = [{src: "fire", meaning: "fire", flipped: false},
- {src: "火", meaning: "fire", flipped: false},
- {src: "water", meaning: "water", flipped: false},
- {src: "水", meaning: "water", flipped: false},
- {src: "earth", meaning: "earth", flipped: false},
- {src: "土", meaning: "earth", flipped: false}
+const kanjiArr = [{name: "fire", meaning: "fire", flipped: false},
+ {name: "火", meaning: "fire", flipped: false},
+ {name: "water", meaning: "water", flipped: false},
+ {name: "水", meaning: "water", flipped: false},
+ {name: "earth", meaning: "earth", flipped: false},
+ {name: "土", meaning: "earth", flipped: false}
 ];
 
 
 export default function App() {
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
+  const [choice1, setChoice1] = useState(null)
+  const [choice2, setChoice2] = useState(null)
   const [turns, setTurns] = useState(0)
 
 
@@ -21,33 +23,71 @@ export default function App() {
     let shuffled = [...kanjiArr].sort(() => Math.random() - 0.5)
     .map((card) => ({...card, id: Math.random()}));
 
+
+
     setCards(shuffled)
     setTurns(0)
   }
   
+  function checkMatch(){
+    if(choice1 && choice2){
+      if (choice1.meaning === choice2.meaning ) {
+        choice1.flipped = true;
+        choice2.flipped = true;
+    
+        setChoice1(null);
+        setChoice2(null);
+      } else {
+        choice1.flipped = false;
+        choice2.flipped = false;
+        setChoice1(null);
+        setChoice2(null);
+      }
+    }
+  }
+  
+  function setChoices(card){
+    if (!choice1 || !choice2){
+      if(!choice1){
+        setChoice1(card)
+      } else {
+        setChoice2(card)
+      }
+    }
+  }
 
-  useEffect(() => {
-  // if they matched => we keep them
-   //else we flip them back, [turn]
-  }, [flipped]);
+  function handleClick(card) {
 
-  useEffect(() => {
-    console.log(turns)
-
-
-  }, [turns]);
-
-  function handleClick() {
-    console.log("card clicked")
-    console.log(cards)
+    if (!card.flipped){
+      card.flipped = true
+      setChoices(card);
+    }
     setTurns(turns + 1)
   }
 
-  function displayCards() {
-   return cards.map(card => {
-       return <ul className = "card" onClick={handleClick} id={card.id} >{card.src}</ul>
-   })
-  }
+  function displayCards() { 
+    return cards.map(card => {
+        return (
+        <div className = "card" key={card.id} onClick={() => handleClick(card)}>
+         <ul className = "card-front" >{card.name}</ul>
+         <ul className = "card-flipped" >{card.name}</ul>
+        </div>)
+    })
+   }
+
+  useEffect(() =>{
+    checkMatch();
+    },[choice1, choice2])
+
+    // useEffect(() => {
+    //   // if they matched => we keep them
+    //    //else we flip them back, [turn]
+    //   }, [flipped]);
+    
+      useEffect(() => {
+    
+      }, [turns]);
+    
   
   return (
       <div> 
@@ -55,7 +95,6 @@ export default function App() {
         {displayCards()}
       </div>
       <button onClick={cardShuffle}>New Game</button>
-
       </div>
       )
 
@@ -64,7 +103,7 @@ export default function App() {
   
 
   
-  // post the images here
+
 
 
 
