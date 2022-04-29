5991015e610b1c37c192490ad633c0d756d7fb96
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-04-29 16:52:27 +0900

    Refactor: update server.js
    Co-authored-by: JonrGull <jonrgull@users.noreply.github.com>
    Co-authored-by: Constantine7s <constantine7s@users.noreply.github.com>

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex(config.development);
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors());
 // app.use(morgan("dev"));
