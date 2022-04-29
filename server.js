eee295d5cd6cb541db7d7d01eb00ad8973a840e1
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-04-29 15:30:50 +0900

    Refactor: update server code
    Co-authored-by: JonrGull <jonrgull@users.noreply.github.com>
    Co-authored-by: Constantine7s <constantine7s@users.noreply.github.com>

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex(config[environment]);
 
 const port = process.env.PORT || 8080;
 
-// app.use(express.static(path.join(__dirname, "/src)));
+app.use(express.static(path.join(__dirname, "./src")));
 
 app.use(express.json());
 
