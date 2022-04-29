b7e8e4066bc287bc36d99b4adca69a3108a86ba9
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-04-29 16:19:33 +0900

    refactor: update server and procfile
    Co-authored-by: JonrGull <jonrgull@users.noreply.github.com>
    Co-authored-by: Constantine7s <constantine7s@users.noreply.github.com>

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: npm run migrate && npm run seed 
-web: npm start && npm run server
+web: npm run server
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex(config.development);
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./src")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 
 app.use(cors());
