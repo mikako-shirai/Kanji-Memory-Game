421dceea50d59282a07fc0c595be14bc43b57082
mikako-shirai <mkkshirai@gmail.com>
2022-05-02 15:27:48 +0900

    feat/refactor: create CardList component and update previous code in App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 import UserScore from "./UserScore.jsx";
-// import CardList from './CardList.jsx';
+import CardList from './CardList.jsx';
 import "../styles/app.css";
 
 const App = () => {
@@ -14,72 +14,44 @@ const App = () => {
 
   const currentScore = useRef(null);
 
-  const getCardsAndShuffle = async () => {
+  const getAllCards = async () => {
     const res = await axios.get("/kanji");
-    const cards = res.data;
-    const shuffledCards = cards.sort(() => 0.5 - Math.random());
+    const allCards = res.data;
+    const shuffledCards = allCards.sort(() => 0.5 - Math.random());
     setCards(shuffledCards);
+  };
+
+  const setNewGame = async () => {
     setTurns(0);
     setFlipped(0);
-
-    displayCards(cards);
   };
 
-  const displayCards = (cards) => {
-    return cards.map((card) => {
-      return (
-        <ul
-          className={`card${card.flipped === true ? "-front" : "-flipped"}`}
-          key={card.id}
-          onClick={() => handleClick(card)}
-        >
-          {card.name}
-        </ul>
-      );
-    });
-  };
-
-  const displayTurns = () => <h2>Turns: {turns}</h2>;
-
-  const checkMatch = () => {
-    if (choice1 && choice2) {
-      if (choice1.meaning === choice2.meaning) {
-        choice1.flipped = true;
-        choice2.flipped = true;
-
-        setChoice1(null);
-        setChoice2(null);
-        setTurns(turns + 1);
-        setFlipped(flipped + 2);
-        console.log("MATCH");
-      } else {
-        setTimeout(() => {
-          choice1.flipped = false;
-          choice2.flipped = false;
-          setTurns(turns + 1);
-          setChoice1(null);
-          setChoice2(null);
-        }, "800");
-        console.log("DIDN'T MATCH");
-      }
+  const cardClickHandler = (card) => {
+    if (!card.flipped) {
+      card.flipped = true;
+      setTurns(turns + 1);
+      setChoices(card);
     }
   };
 
   const setChoices = (card) => {
-    if (!choice1 || !choice2) {
-      if (!choice1) {
-        setChoice1(card);
-      } else {
-        setChoice2(card);
-      }
+    if (!choice1) {
+      setChoice1(card);
+    } else {
+      setChoice2(card);
+      checkMatch();
     }
   };
 
-  const handleClick = (card) => {
-    if (!card.flipped) {
-      card.flipped = true;
-      setChoices(card);
-    }
+  const checkMatch = () => {
+    choice1.flipped = choice1.meaning === choice2.meaning;
+    choice2.flipped = choice1.meaning === choice2.meaning;
+
+    if (choice1.flipped) setFlipped(flipped + 2);
+    console.log(choice1.flipped ? "MATCH" : "DIDN'T MATCH");
+
+    setChoice1(null);
+    setChoice2(null);
   };
 
   const displayLeaderBoard = () => {
@@ -111,28 +83,25 @@ const App = () => {
   }
 
   useEffect(() => {
-    checkMatch();
-  }, [choice1, choice2]);
-
-  useEffect(() => {
-    if (flipped === cards.length) {
-      console.log("YOU WON");
-    }
+    if (flipped === cards.length) console.log("YOU WON");
   }, [flipped]);
 
   return (
     <div>
       <h1>Kanji Memory Game</h1>
-      <button className="new-game-btn" onClick={getCardsAndShuffle}>
+      <button className="new-game-btn" onClick={() => {
+        getAllCards();
+        setNewGame();
+      }}>
         New Game
       </button>
 
       {flipped === cards.length && displayLeaderBoard()}
 
-      <div className="card-display">{displayCards(cards)}</div>
+      <div className="card-display"><CardList cards={cards} cardClickHandler={cardClickHandler} /></div>
 
       {cards.length > 0 && (
-        <div className="display-turns">{displayTurns()}</div>
+        <div className="display-turns"><h2>Turns: {turns}</h2></div>
       )}
     </div>
   );
--- src/Components/cardList.jsx
+++ src/Components/cardList.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 // import Card from './Card.jsx.js';
 
-const CardList = (cards) => {
+const CardList = ({ cards, cardClickHandler }) => {
+  const [showCards, setShowCards] = useState(false);
+
+  useEffect(() => {
+    setShowCards(true);
+  }, [cards]);
+
   return(
     <div className='card-grid'>
-      {cards.map(card => <div card={card} key={card.meaning}>card</div>)}
+      {showCards && cards.map((card) => {
+        return (
+          <ul
+            className={`card${card.flipped ? "-front" : "-flipped"}`}
+            key={card.id}
+            onClick={() => cardClickHandler(card)}
+          >
+            {card.name}
+          </ul>
+        )})
+      }
     </div>
+
+
+
+    // <div className='card-grid'>
+    //   {cards.map(card => <div card={card} key={card.meaning}>card</div>)}
+    // </div>
   );
 };       
 
