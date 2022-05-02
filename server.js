48e32b05fb0404822e2399246410af3822d9ed75
mikako-shirai <mkkshirai@gmail.com>
2022-05-02 16:59:01 +0900

    chore: update files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 require("dotenv").config({
   path: "./.env.local",
 });
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV ? "development" : "production";
 const config = require("./knexfile");
 const db = knex(config[environment]);
 
