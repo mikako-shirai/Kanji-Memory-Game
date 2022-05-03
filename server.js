8b1b543b9824ad865779ce8780f66f7d30695b6e
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-03 11:07:16 +0900

    chore: remove ternery operator from environment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config({
   path: "./.env.local",
 });
 
-const environment = process.env.NODE_ENV ? "production" : "development";
+const environment = process.env.NODE_ENV;
 const config = require("./knexfile");
 const db = knex(config[environment]);
 const port = process.env.PORT || 8080;
