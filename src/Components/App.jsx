467f0e00250f64f4d4b1828fe1220eef1b3f6533
mikako-shirai <mkkshirai@gmail.com>
2022-05-03 14:25:36 +0900

    feat: add SubmitName feature

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 import CardList from './CardList.jsx';
 import SubmitName from './SubmitName.jsx';
-
 import LeaderBoard from "./LeaderBoard.jsx";
 import "../styles/app.css";
 
