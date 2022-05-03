808dbdc5f0cad075b2a50d07ea57db924acb210a
stk-m <stk.murakami@gmail.com>
2022-05-03 11:40:06 +0900

    feat: add axios in LeaderBoard.jsx

--- src/Components/LeaderBoard.jsx
+++ src/Components/LeaderBoard.jsx
@@ -1,41 +1,30 @@
 import React from "react";
 import axios from "axios";
 
-function LeaderBoard() {
-//const res = await axios.get("/leaderboard");
-//console.log(res.data)
-  // const users = res.data;
-
+const LeaderBoard = async ()=> {
+  const res = await axios.get("/leaderboard");
+  const leadUsers = [...res.data];
+  console.log(leadUsers)
   //leaderboard table:("id").("name", 32).("score")
 
-  const sampleUsers = [
-    {id:1, name: "Miguel", score:5},
-    {id:2, name: "Michel", score:4},
-    {id:3, name: "Michiko", score:3}
-  ]
+  // const sampleUsers = [
+  //   {id:1, name: "Miguel", score:5},
+  //   {id:2, name: "Michel", score:4},
+  //   {id:3, name: "Michiko", score:3}
+  // ]
 
   return (
     <div className="leader-board">
-      <table className="board-table">
-        <thead>
-          <tr>
-              <th >leader board</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {sampleUsers.map((el, key)=>
-            <div className="lanking" key ={key}>
-              <td className="user-name">&#128081;{el.name}</td>
-              <td className="user-score">{el.score}</td>
-            </div>
-            )}
-          </tr>
-        </tbody>
-      </table>
+      <div>leader board</div>
+        {leadUsers.map((user, key)=>
+        (<div className="lanking" key ={key}>
+          <div className="user-name">{console.log(user.name)}</div>
+          <div className="user-score">score:{user.score}</div>
+        </div>)
+        )}
     </div>
-  );
-}
+  )
+};
 
 export default LeaderBoard;
 
