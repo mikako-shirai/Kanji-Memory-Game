e0920f4ff519d7bf4a9684cc89f321d73ba1f4bf
mikako-shirai <mkkshirai@gmail.com>
2022-05-03 15:24:22 +0900

    feat: add SubmitName feature

--- src/Components/SubmitName.jsx
+++ src/Components/SubmitName.jsx
@@ -17,16 +17,14 @@ const SubmitName = ({ turns, setShowSubmitName }) => {
 
   return (
     <div className="score-board">
-      <div className="score">
-        <h5>You won!</h5>
-        <h5>Enter your name!</h5> 
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={submitScore}>Submit</button>
-      </div>
+      <h5>You won!</h5>
+      <h5>Enter your name!</h5> 
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={submitScore}>Submit</button>
     </div>
   );
 };
